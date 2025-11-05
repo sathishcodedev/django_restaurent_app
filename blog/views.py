ece744634ea8e6e0ef74e817de93c850ab9dd4a9
sathishcodedev <sathishx468@gmail.com>
2025-11-05 14:11:46 +0530

    adding herosection, header and fotter section create seperately

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 # first view testing how its work
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
--- foodapp/settings.py
+++ foodapp/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'foodapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
