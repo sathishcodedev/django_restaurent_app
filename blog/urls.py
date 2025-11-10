d6205849565bc428ba446645876bc163f96d2363
sathishcodedev <sathishx468@gmail.com>
2025-11-10 11:56:14 +0530

    detail view were create to food id call to show seperatelly

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('',views.index, name="index_page"),
+    path('detail/<int:id>/',views.detailview, name='detail_page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,23 @@ from .models import Post
 
 
 
-
 # first view testing how its work
 def index(request):
+    #all data get from database post_table
     posts = Post.objects.all()
 
-    return render(request, 'index.html',{'posts':posts})
+    category = request.GET.get('category')
+    if category:
+        post_category = Post.objects.filter(category = category)
+    else:
+        post_category = Post.objects.all()[:12]
+    return render(request, 'index.html',{'posts':post_category, 'post':posts})
+
+
+def detailview(request, id):
+
+    posts = Post.objects.all()
+    
+    detail_category = Post.objects.filter(category=id)
+
+    return render(request, 'detail.html', {'posts':detail_category})
